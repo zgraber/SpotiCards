a1ed0a2c3e1e00170b302bf35d768fc0db4f17eb
Zak Graber <zakgraber@gmail.com>
2020-04-11 15:41:50 -0700

    Created a create game view. Created a route to add game to db. Refactored routes

--- routes/game.js
+++ routes/game.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var SpotifyWebApi = require('spotify-web-api-node');
+var db_url = process.env.DB_URL;
+var MongoClient = require('mongodb').MongoClient;
+
 
 var spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
@@ -8,8 +11,7 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URI
 });
 
-var questions = [
-    {
+var questions = [{
         question_id: 0,
         text: "Which player listens to the most danceable music?",
         points: 100
@@ -30,24 +32,68 @@ function getAnswers(req) {
 
 }
 
-router.get('/', function (req, res) {
-    /*
-    req.session.players.forEach((player) =>{
-        spotifyApi.setAccessToken(player.access_token);
-        spotifyApi.getMe()
-            .then(function (data) {
-                console.log('Some information about the authenticated user', data.body);
-            }, function (err) {
-                console.log('Something went wrong!', err);
-            });
-    });
-    res.send(questions[0]['text']);
-    */
+function generateUrlId() {
+    var id = '';
+    var arr = '1234567890abcdefghijklmnopqrstuvwxyz';
+    var len = 7;
+    for (var i = len; i > 0; i--) {
+        id += arr[Math.floor(Math.random() * arr.length)];
+    }
+    return id;
+}
 
+function generateGameCode() {
+    var code = '';
+    var arr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var len = 4;
+    for (var i = len; i > 0; i--) {
+        code += arr[Math.floor(Math.random() * arr.length)];
+    }
+    return code;
+}
+
+router.get('/:id/lobby', function(req, res) {
+    let parms = {title:'Lobby', active: {players: false}};
+    if (req.session.players) {
+        parms.active.players = true;
+        parms.players = req.session.players;
+    } 
+    res.render('lobby', parms);
+});
+
+router.get('/', function (req, res) {
     req.session.question_ids = [2, 0, 1];
     req.session.active_question = 0;
     getAnswers(req);
     res.send(questions[req.session.question_ids[req.session.active_question]].text);
+});
+
+//Create a game
+router.post('/', function (req, res) {
+    var url_id = generateUrlId();
+    var game_code = generateGameCode();
+    game = {
+        url_id: url_id,
+        game_code: game_code,
+        question_ids: [],
+        answers: [],
+        active_question: -1,
+        players: []
+    };
+
+    MongoClient.connect(db_url, function (err, db) {
+        if (err) {
+            return res.next(err);
+        }
+        var dbo = db.db('SpotiParty');
+        dbo.collection("Games").insertOne(game, function (err, res) {
+            if (err) return res.next(err);
+            console.log("Game with code " + game_code + " added to db");
+            db.close();
+        });
+    });
+
+    res.redirect('/game/' + url_id + '/lobby');
 });
 
 module.exports = router;
