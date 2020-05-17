ef48ec664e41a381cdadaf73945ef4598caf9b81
Zak Graber <zakgraber@gmail.com>
2020-05-17 00:02:24 -0700

    Fixed bug where name would be added multiple times on refresh of confirm page

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,13 +1,13 @@
 var socket = io();
 
-function loadPlayerNames(){
+function loadPlayerNames() {
     let id = window.location.pathname.split('/')[2];
     let url = window.location.origin + '/game/' + id + '/players';
     $.ajax({
         url: url,
         success: (result) => {
-            if(result.player_names.length > 0) {
-                for (let i=0; i < result.player_names.length; i++) {
+            if (result.player_names.length > 0) {
+                for (let i = 0; i < result.player_names.length; i++) {
                     $('<li></li>', {
                         id: ('player' + i),
                         class: "list-group-item",
@@ -21,21 +21,31 @@ function loadPlayerNames(){
     });
 }
 
-$(document).ready(()=>{
+$(document).ready(() => {
     $("#player-list").hide();
     loadPlayerNames();
-    socket.emit('register screen',{
+    socket.emit('register screen', {
         game_code: $('#game-code').text()
     });
     socket.on('player join', (data) => {
-        if($("#no-players").is(":visible")){
+        if ($("#no-players").is(":visible")) {
             $('#no-players').hide();
             $('#player-list').show();
         }
-        console.log(data.player_name);
-        $('<li></li>', {
-            class: "list-group-item",
-            text: data.player_name,
-        }).appendTo('#player-list');
+
+        let found = false;
+
+        selector = `#player-list :contains('${data.player_name}')`;
+        selectedList = $(selector);
+
+        if (selectedList.length) {
+            found = true;
+        }
+        if (!found) {
+            $('<li></li>', {
+                class: "list-group-item",
+                text: data.player_name,
+            }).appendTo('#player-list');
+        }
     });
 })
